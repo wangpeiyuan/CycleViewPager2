6636c45aff164dd3d28a2ada3c09f61b4aef5fac
wangpeiyuan <wpy0830@gmail.com>
2019-12-02 11:19:00 +0800

    feat: 初始化项目

---
+++ app/src/main/java/com/wangpeiyuan/cycleviewpager2/simple/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wangpeiyuan.cycleviewpager2.simple
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
